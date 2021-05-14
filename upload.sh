5e5e397de07acdc2ffec95833ea3beacabd2f8c0
Leon Minasyan <ierehon1905@gmail.com>
2021-05-14 23:32:57 +0300

    new upload

--- upload.sh
+++ upload.sh
@@ -2,11 +2,17 @@ PASS='FR33d0m13#&'
 
 rsync --delete -havzP --exclude='/.git' \
  --filter="dir-merge,- ./back/.gitignore" \
-  --filter="dir-merge,- ./front/.gitignore" \
-   . root@65.21.155.166:~/nsd052021
+   ./back/* root@65.21.155.166:~/nsd052021/back
 
+# Не забудь забилдить фронт
+# На серваке директоря отдаетсся как статика с помощью serve
+rsync --delete -havzP  \
+   ./front/build/* root@65.21.155.166:~/nsd052021/front
+
+#  --filter="dir-merge,- ./back/.gitignore" \
+#   --filter="dir-merge,- ./front/.gitignore" \
 # # ps ax | grep test.py
 
 # ssh root@65.21.155.166 '/bin/bash -c "source ~/.pipenv/bin/activate && cd ~/nsd052021/back && pip install -r requirements.txt && uvicorn main:app --reload"'
-# ssh root@65.21.155.166 "cd ~ && cd nsd052021/front && yarn install --frozen-lockfile && yarn start"
+# ssh root@65.21.155.166 "cd ~ && cd nsd052021/front && yarn install --frozen-lockfile"
  
